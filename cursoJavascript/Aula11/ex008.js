3b96da00e6c4f21bce440579e54c3588a9da6c7f
Gabriel Cortes <gabriel.a.rocha23@icloud.com>
2025-02-10 07:35:06 -0300

    Aula de Condicoes parte 01

---
+++ cursoJavascript/Aula11/ex008.js
@@ -0,0 +1,10 @@
+var vel = 60.5
+
+if (vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!!')
+} else {
+    console.log(`A velocidade do seu carro é ${vel}hm/h`)
+}
+
+console.log(`Dirija sempre usando cinto de segurança!`)
+
