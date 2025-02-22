bb3f4025e4738a687e3d667d9112eba0f2ae14ca
Gabriel Cortes <gabriel.a.rocha23@icloud.com>
2025-02-21 22:29:14 -0300

    Finalizando Curso Básico de Javascript

--- cursoJavascript/ModuloF/Desafio01/js/main.js
+++ cursoJavascript/ModuloF/Desafio01/js/main.js
@@ -28,6 +28,7 @@ function adicionar() {
         let item = document.createElement('option')
         item.text = `Valor ${txtN.value} adicionado.`
         listaN.appendChild(item)
+        res.innerHTML = ''
     } else {
         window.alert('Valor inválido ou já encontrado na lista!')
     }
@@ -44,8 +45,26 @@ function finalizar() {
         window.alert('Adicione variaves antes de finalizar')
     } else {
         let total = valores.length
-
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = valores[0]
+        let media = 0
+        for (let pos in valores) {
+                soma += valores[pos]
+                if (valores[pos] > maior) {
+                    maior = valores[pos]
+                }
+                 if (valores[pos] < menor) {
+                    menor = valores[pos]
+                }
+           media = soma / total     
+        }
+        
         res.innerHTML = ''
         res.innerHTML += `<p>Ao todo, temos ${total} de números cadastrados </p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}</p>`
+        res.innerHTML += `<p>A media dos valores é ${media}</p>`
     }
 }
